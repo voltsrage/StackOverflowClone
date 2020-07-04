641fd212ac5f7f2930d6808ce05f2feff6db1e6b
Trendon Cato <electric5388@hotmail.com>
2020-07-04 16:05:01 +0800

    Added Models and DbContext

---
+++ StackOverflowProject/StackOverflowProject.DomainModels/StackOverflowDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StackOverflowProject.DomainModels
+{
+    public class StackOverflowDBContext:DbContext
+    {
+        public StackOverflowDBContext():base("StackOverflowDatabaseDbContext")
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ StackOverflowProject/StackOverflowProject.DomainModels/Vote.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflowProject.DomainModels
+{
+    public class Vote
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int VoteID { get; set; }
+
+        public int UserID { get; set; }
+
+        public int AnswerID { get; set; }
+
+        public int VoteValue { get; set; }
+
+        [ForeignKey("AnswerID ")]
+        public virtual Answer Answer { get; set; }
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+    }
+}
